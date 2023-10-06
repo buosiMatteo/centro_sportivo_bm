8405a611c1500fe3cf34b9d8ee56811ac7a764d1
matt <buosi.matteo@gmail.com>
2023-10-06 09:01:55 +0200

    Fixed annotation in main file

--- src/main/java/it/euris/centrosportivobm/CentroSportivoBmApplication.java
+++ src/main/java/it/euris/centrosportivobm/CentroSportivoBmApplication.java
@@ -1,9 +1,16 @@
 package it.euris.centrosportivobm;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Centro Sportivo Project API", version = "1.0", description = "Centro sportivo management"))
+@SecurityScheme(name = "authentication", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class CentroSportivoBmApplication {
 
 	public static void main(String[] args) {
